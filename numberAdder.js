35ab0bf1fcda79055bf76a39dfb9e6b6a966ed38
drewpchannel <drewpcchannel@gmail.com>
2017-04-12 14:31:35 -0500

    added cli option to code

--- numberAdder.js
+++ numberAdder.js
@@ -1,13 +1,15 @@
 (() => {
   let sampleArray = ['4916-2600-1804-0530', '4779-252088-3072', '4252-278093-7078', '4556-4242-9283-2260'];
-  if(!process.argv[2]) {
+  //Quick check to see if there are numbers added in the command line
+  if(process.argv[2]) {
     sampleArray = [];
     let j = 2;
-    while(!process.argv[j]) {
+    while(process.argv[j]) {
       j++;
-      sampleArray.push(process.argv[j]);
+      (process.argv[j]) ? sampleArray.push(process.argv[j]) : 0;
     }
   }
+  //The actual code that sorts the numbers
   let results = [0, sampleArray[0]];
   sampleArray.forEach( (elem, index) => {
     let cardSum = 0;
